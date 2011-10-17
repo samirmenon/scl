0c438335633927ee79f85617beaa85edd786d4ab
Samir <smenon@stanford.edu>
2011-10-17 16:54:27 -0400

    Updating the code

--- applications-linux/scl_nw_conn/scl_nw_conn.cpp
+++ applications-linux/scl_nw_conn/scl_nw_conn.cpp
@@ -10,7 +10,12 @@
 
 #include <PracticalSocket.h>
 
-#include <scl/CSystemClock.hpp>
+#include <Eigen/Core>
+
+#include <sutil/CSystemClock.hpp>
+
+#define N_TRAN 10
+#define N_RECV 300
 
 
 /** A sample network communication app */
@@ -27,6 +32,40 @@ int main(int argc, char** argv)
     try
     {
       std::cout<<"\nStart Time:"<<sutil::CSystemClock::getSysTime()<<"\n";
+
+      //Set up a client socket:
+      TCPSocket cli_sock("172.0.0.1",8080);
+
+      double t[N_TRAN]; //Transmit 10 values
+      double r[N_RECV]; //Recv 300 values
+
+      //Then send/recv the stuff: 10 values, 10 times
+      for(unsigned int i=0;i<10;++i)
+      {
+        //First set the 10 values to the time stamps
+        t[0] = N_TRAN;
+        for(unsigned int j=1;j<N_TRAN;++j)
+        { t[j] = sutil::CSystemClock::getSysTime(); }
+
+        flag = cli_sock.send(t,N_TRAN*sizeof(double));
+        if (true == flag)
+        {
+          std::cout<<"\nSent ["<<N_TRAN<<" doubles] : ";
+          for(unsigned int j=0;j<N_TRAN;++j)
+            std::cout<<t[j]<<", ";
+        }
+        else{ throw("Send failed"); }
+
+        r[0] = N_RECV;
+        flag = cli_sock.recv(r,N_RECV*sizeof(double));
+        if (true == flag)
+        {
+          std::cout<<"\nRecv ["<<N_RECV<<" doubles] : ";
+          for(unsigned int j=0;j<N_RECV;++j)
+            std::cout<<r[j]<<", ";
+        }
+        else{ throw("Recv failed"); }
+      }
 
       std::cout<<"\nEnd Time:"<<sutil::CSystemClock::getSysTime()<<"\n";
       return 0;
