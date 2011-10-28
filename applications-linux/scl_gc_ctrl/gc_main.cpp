bb7b12aca6b0bf05f2d341612b040450ffca626e
Samir <smenon@stanford.edu>
2011-10-28 11:39:58 -0700

    Changing Busylizzy to scl (in apps)

--- applications-linux/scl_gc_ctrl/gc_main.cpp
+++ applications-linux/scl_gc_ctrl/gc_main.cpp
@@ -1,7 +1,7 @@
-/* This file is part of Busylizzy, a control and simulation library
+/* This file is part of scl, a control and simulation library
 for robots and biomechanical models.
 
-Busylizzy is free software; you can redistribute it and/or
+scl is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
 License as published by the Free Software Foundation; either
 version 3 of the License, or (at your option) any later version.
@@ -11,14 +11,14 @@ modify it under the terms of the GNU General Public License as
 published by the Free Software Foundation; either version 2 of
 the License, or (at your option) any later version.
 
-Busylizzy is distributed in the hope that it will be useful,
+scl is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public
 License and a copy of the GNU General Public License along with
-Busylizzy. If not, see <http://www.gnu.org/licenses/>.
+scl. If not, see <http://www.gnu.org/licenses/>.
 */
 /* \file gc_main.cpp
  *
