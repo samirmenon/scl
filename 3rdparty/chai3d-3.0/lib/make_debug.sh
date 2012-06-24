0e44446f0367ce266d80844330a83cc82dfa8b77
Samir <smenon@stanford.edu>
2012-06-24 12:16:32 -0700

    Adding build files for chai3

---
+++ 3rdparty/chai3d-3.0/lib/make_debug.sh
@@ -0,0 +1,8 @@
+rm debug -rf &&
+mkdir -p debug &&
+mkdir -p build_dbg &&
+cd build_dbg &&
+cmake .. -DCMAKE_BUILD_TYPE=Debug &&
+make -j8 &&
+mv lib* ../debug/ &&
+cd ..
---
+++ 3rdparty/chai3d-3.0/lib/make_release.sh
@@ -0,0 +1,8 @@
+rm release -rf &&
+mkdir -p release &&
+mkdir -p build_rel &&
+cd build_rel &&
+cmake .. -DCMAKE_BUILD_TYPE=Release &&
+make -j8 &&
+mv lib* ../release/ &&
+cd ..
