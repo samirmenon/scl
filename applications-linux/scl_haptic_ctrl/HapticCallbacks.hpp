dfcc1eaebf00255586c51e64e55bd903d06192f8
Samir <smenon@stanford.edu>
2012-09-03 17:17:39 -0700

    Removing unnecessary files

--- applications-linux/scl_haptic_ctrl/HapticCallbacks.hpp
+++ applications-linux/scl_haptic_ctrl/HapticCallbacks.hpp
@@ -53,41 +53,6 @@ namespace scl_app
     try
     {
       /** ************************************************************************
-       * Keyboard keys for the command line shell:
-       *
-       * Associate keyboard keys as the key handlers for
-       * ui_point_1's x, y and z values. ui_point_1 can
-       * then be used in any part of the program for other
-       * stuff (like controlling an operational point)
-       * ************************************************************************ */
-      flag = sutil::callbacks::add<scl::CCallbackDecrement,char,bool,double>(
-          'w', &(scl::CDatabase::getData()->s_gui_.ui_point_[0](0)) );
-      flag = flag && sutil::callbacks::add<scl::CCallbackIncrement,char,bool,double>(
-          's', &(scl::CDatabase::getData()->s_gui_.ui_point_[0](0)) );
-      flag = flag && sutil::callbacks::add<scl::CCallbackDecrement,char,bool,double>(
-          'a', &(scl::CDatabase::getData()->s_gui_.ui_point_[0](1)) );
-      flag = flag && sutil::callbacks::add<scl::CCallbackIncrement,char,bool,double>(
-          'd', &(scl::CDatabase::getData()->s_gui_.ui_point_[0](1)) );
-      flag = flag && sutil::callbacks::add<scl::CCallbackDecrement,char,bool,double>(
-          'q', &(scl::CDatabase::getData()->s_gui_.ui_point_[0](2)) );
-      flag = flag && sutil::callbacks::add<scl::CCallbackIncrement,char,bool,double>(
-          'e', &(scl::CDatabase::getData()->s_gui_.ui_point_[0](2)) );
-
-      flag = flag && sutil::callbacks::add<scl::CCallbackDecrement,char,bool,double>(
-          'u', &(scl::CDatabase::getData()->s_gui_.ui_point_[1](0)) );
-      flag = flag && sutil::callbacks::add<scl::CCallbackIncrement,char,bool,double>(
-          'j', &(scl::CDatabase::getData()->s_gui_.ui_point_[1](0)) );
-      flag = flag && sutil::callbacks::add<scl::CCallbackDecrement,char,bool,double>(
-          'h', &(scl::CDatabase::getData()->s_gui_.ui_point_[1](1)) );
-      flag = flag && sutil::callbacks::add<scl::CCallbackIncrement,char,bool,double>(
-          'k', &(scl::CDatabase::getData()->s_gui_.ui_point_[1](1)) );
-      flag = flag && sutil::callbacks::add<scl::CCallbackDecrement,char,bool,double>(
-          'y', &(scl::CDatabase::getData()->s_gui_.ui_point_[1](2)) );
-      flag = flag && sutil::callbacks::add<scl::CCallbackIncrement,char,bool,double>(
-          'i', &(scl::CDatabase::getData()->s_gui_.ui_point_[1](2)) );
-      if(false == flag){throw(std::runtime_error("Could not add a keyboard callback"));  }
-
-      /** ************************************************************************
        * Add a help function to the command line shell
        * *************************************************************************/
       flag = sutil::callbacks::add<scl::CCallbackHelp, std::string, std::vector<std::string> >(
@@ -108,6 +73,9 @@ namespace scl_app
           std::string("print") );
       if(false == flag){throw(std::runtime_error("Could not add a print callback"));  }
 
+      /** ************************************************************************
+       * Add all the printables : Stuff that can be printed by the print callback.
+       * *************************************************************************/
       flag = scl::addRobotPrintables();
       if(false == flag){throw(std::runtime_error("Could not add callbacks to print robot info"));  }
     }
