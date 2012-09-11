df6eddd393926fa8fc51c9f3918e72a97f2baf6b
Samir <smenon@stanford.edu>
2012-09-10 22:10:55 -0700

    Updating a bug in chai

--- 3rdparty/chai3d-3.0/chai3d/math/CTransform.h
+++ 3rdparty/chai3d-3.0/chai3d/math/CTransform.h
@@ -476,7 +476,7 @@ struct cTransform
                      m[2][3] * a_matrix.m[0][2] + m[3][3] * a_matrix.m[0][3];
         double m31 = m[0][3] * a_matrix.m[1][0] + m[1][3] * a_matrix.m[1][1] +
                      m[2][3] * a_matrix.m[1][2] + m[3][3] * a_matrix.m[1][3];
-        double m32 = m[0][3] * a_matrix.m[2][1] + m[1][3] * a_matrix.m[2][1] +
+        double m32 = m[0][3] * a_matrix.m[2][0] + m[1][3] * a_matrix.m[2][1] +
                      m[2][3] * a_matrix.m[2][2] + m[3][3] * a_matrix.m[2][3];
         double m33 = m[0][3] * a_matrix.m[3][0] + m[1][3] * a_matrix.m[3][1] +
                      m[2][3] * a_matrix.m[3][2] + m[3][3] * a_matrix.m[3][3];
@@ -537,7 +537,7 @@ struct cTransform
                      m[2][3] * a_matrix.m[0][2] + m[3][3] * a_matrix.m[0][3];
         double m31 = m[0][3] * a_matrix.m[1][0] + m[1][3] * a_matrix.m[1][1] +
                      m[2][3] * a_matrix.m[1][2] + m[3][3] * a_matrix.m[1][3];
-        double m32 = m[0][3] * a_matrix.m[2][1] + m[1][3] * a_matrix.m[2][1] +
+        double m32 = m[0][3] * a_matrix.m[2][0] + m[1][3] * a_matrix.m[2][1] +
                      m[2][3] * a_matrix.m[2][2] + m[3][3] * a_matrix.m[2][3];
         double m33 = m[0][3] * a_matrix.m[3][0] + m[1][3] * a_matrix.m[3][1] +
                      m[2][3] * a_matrix.m[3][2] + m[3][3] * a_matrix.m[3][3];
