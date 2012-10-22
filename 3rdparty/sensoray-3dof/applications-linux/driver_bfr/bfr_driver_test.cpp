556cfc7a589cceaeba08d5860dedc80357f02113
Samir <smenon@stanford.edu>
2012-10-22 15:13:52 -0700

    Using the new init function in the driver to handle stuff

--- 3rdparty/sensoray-3dof/applications-linux/driver_bfr/bfr_driver_test.cpp
+++ 3rdparty/sensoray-3dof/applications-linux/driver_bfr/bfr_driver_test.cpp
@@ -24,37 +24,18 @@ extern "C" {
 
 int main()
 {
+  bool flag;
   sensoray::CSensoray3DofIODriver sensorayio;
-	u32 faults;
 
-	// Open the 2600 api.  Declare one MM in system.
-	if ( ( faults = S26_DriverOpen( 1 ) ) != 0 )
-	{
-		printf( "DriverOpen() fault: %d\n", (int)faults );
-	}
-	else
-	{
-		// Open the MM.
-		if ( ( faults = S26_BoardOpen( sensorayio.getData().mm_handle_, 0, sensorayio.getData().mm_ip_addr_.c_str() ) ) != 0 )
-			printf( "BoardOpen() fault: %d\n", (int)faults );
+  flag = sensorayio.init();
+  if(false == flag)
+  { return 1; }
 
-		// If MM was successfully opened ...
-		else
-		{
-			// Reset the I/O system.
-			S26_ResetNetwork( sensorayio.getData().mm_handle_ );
+  // Execute the i/o control loop until it terminates.
+  sensorayio.ioControlMain();
 
-			// Register all iom's.  If no errors, execute the I/O control loop until it is terminated.
-			if ( sensorayio.detectAllIoms() )
-			{
-				// Execute the i/o control loop until it terminates.
-			  sensorayio.ioControlMain();
-			}
-		}
-
-		// Close the api library.
-		S26_DriverClose();
-	}
+  // Close the api library.
+  S26_DriverClose();
 
 	return 0;
 }
