0a4331f10b2a39689c124fb797d4857306c2aa4d
Samir <smenon@stanford.edu>
2012-10-22 20:23:59 -0700

    Adding csystemclock. Also adding an encoder only test.

--- 3rdparty/sensoray-3dof/applications-linux/driver_bfr/bfr_driver_test.cpp
+++ 3rdparty/sensoray-3dof/applications-linux/driver_bfr/bfr_driver_test.cpp
@@ -17,8 +17,11 @@ extern "C" {
 
 #include <sensoray/CSensoray3DofIODriver.hpp>
 
+#include <sutil/CSystemClock.hpp>
+
 #include <string>
 #include <stdio.h>
+#include <iostream>
 
 // EXECUTABLE ////////////////////////////////////////////////////////////////////
 
@@ -29,21 +32,38 @@ int main()
   time_t  t_start;    // Benchmark start time.
   double  t_tot;     // Benchmark elapsed time.
 
+  flag = sutil::CSystemClock::start();
+  if(false == flag)
+  {
+    std::cout<<"\nCould not start clock.\n";
+    return 1;
+  }
+
   sensoray::CSensoray3DofIODriver sensorayio;   // Create a driver object
 
   flag = sensorayio.init();                     // Initialize the driver
   if(false == flag)  { return 1; }
 
-  t_start = time( NULL );
+  // TEST 1 : Run a full control loop
+  t_start = sutil::CSystemClock::getSysTime();
   ctrl_cycles = sensorayio.ioControlLoop();    // Run control loop until terminated or error.
-  t_tot = difftime( time( NULL ), t_start );
-
-  sensorayio.shutdown();                       // Shut down the driver
+  t_tot = sutil::CSystemClock::getSysTime() - t_start;
 
   // Report benchmark results.
   printf( "\nControl loop cycles:    %d", ctrl_cycles );
   printf( "\nElapsed time (seconds): %lf", t_tot );
   printf( "\nAverage I/O cycle time (msec):  %.2f \n\n", t_tot / static_cast<double>(ctrl_cycles) * 1000.0 );
+
+  // TEST 2 : Test encoders
+  t_start = sutil::CSystemClock::getSysTime();
+  for(int i=0;i<300; i++)
+  { ctrl_cycles = sensorayio.readEncoders();  }
+  t_tot = sutil::CSystemClock::getSysTime() - t_start;
+
+  std::cout<<"\nEncoders read 300 times. Time taken = "<<t_tot<<"\n\n"<<std::flush;
+
+  // Exit
+  sensorayio.shutdown();                       // Shut down the driver
 
 	return 0;
 }
