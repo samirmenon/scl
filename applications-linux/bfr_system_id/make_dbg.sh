3653f1a7e6de9ca1070997bde4507c9df7c7defb
Samir Menon <smenon@stanford.edu>
2013-04-15 21:26:56 -0700

    Also compiles the nw bfr identification code

--- applications-linux/bfr_system_id/make_dbg.sh
+++ applications-linux/bfr_system_id/make_dbg.sh
@@ -3,4 +3,5 @@ cd build_dbg &&
 cmake .. -DCMAKE_BUILD_TYPE=Debug &&
 make -j8 &&
 cp -rf bfr_system_id ../ &&
+cp -rf bfr_nw_system_id ../ &&
 cd ..
--- applications-linux/bfr_system_id/make_rel.sh
+++ applications-linux/bfr_system_id/make_rel.sh
@@ -3,4 +3,5 @@ cd build_rel &&
 cmake .. -DCMAKE_BUILD_TYPE=Release &&
 make -j8 &&
 cp -rf bfr_system_id ../ &&
+cp -rf bfr_nw_system_id ../ &&
 cd ..
