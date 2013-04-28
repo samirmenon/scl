9e9e7456f8e9646c5fa6f575f20a70d6582649c8
Samir Menon <smenon@stanford.edu>
2013-04-28 00:35:25 -0700

    Updating the build scripts for the new binaries

--- 3rdparty/sensoray-3dof/applications-linux/driver_bfr/make_dbg.sh
+++ 3rdparty/sensoray-3dof/applications-linux/driver_bfr/make_dbg.sh
@@ -3,4 +3,6 @@ cd build_dbg &&
 cmake .. -DCMAKE_BUILD_TYPE=Debug &&
 make -j8 &&
 cp -rf bfr_driver_test ../ &&
+cp -rf bfr_driver_fx ../ &&
+cp -rf bfr_driver_raw_input ../ &&
 cd ..
--- 3rdparty/sensoray-3dof/applications-linux/driver_bfr/make_rel.sh
+++ 3rdparty/sensoray-3dof/applications-linux/driver_bfr/make_rel.sh
@@ -3,4 +3,6 @@ cd build_rel &&
 cmake .. -DCMAKE_BUILD_TYPE=Release &&
 make -j8 &&
 cp -rf bfr_driver_test ../ &&
+cp -rf bfr_driver_fx ../ &&
+cp -rf bfr_driver_raw_input ../ &&
 cd ..
