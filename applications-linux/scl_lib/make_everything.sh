2abd70ec86a2ae10591ea2229b3c92fc1fe008b4
Samir <smenon@stanford.edu>
2013-07-22 05:08:58 -0700

    Updating the make everything script

--- applications-linux/scl_lib/make_everything.sh
+++ applications-linux/scl_lib/make_everything.sh
@@ -24,9 +24,9 @@ cd ../scl_test &&
 #rm -rf build_* &&
 sh make_debug.sh &&
 sh make_release.sh &&
-cd ../scl_dynamics && make release -j8 &&
-cd ../scl_gc_ctrl && make release -j8 &&
-cd ../scl_task_ctrl && make release -j8 &&
+cd ../scl_dynamics && sh make_dbg.sh && sh make_rel.sh &&
+cd ../scl_gc_ctrl && sh make_dbg.sh && sh make_rel.sh &&
+cd ../scl_task_ctrl && sh make_dbg.sh && sh make_rel.sh &&
 cd ../scl_example_ctrl && sh make_dbg.sh && sh make_rel.sh &&
 cd ../scl_file_converter && make release -j8 &&
 cd ../scl_haptic_ctrl && sh make_dbg.sh && sh make_rel.sh &&
