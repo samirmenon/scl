3d1bb7a7bf7dc0da163d9bf8c2b60b6dca124d82
Samir <smenon@stanford.edu>
2013-08-21 04:33:06 -0700

    Removing some junk functions from root node

--- src/scl/dynamics/tao/tao/dynamics/taoABNode.h
+++ src/scl/dynamics/tao/tao/dynamics/taoABNode.h
@@ -54,52 +54,53 @@ public:
 	virtual deVector6* A() { return &_A; }
 
 	// Ia = I
-	virtual void abInertiaInit(deMatrix6& Ia) = 0;
+	virtual void abInertiaInit(deMatrix6& Ia) {}
 
 	// Ya = -Xc Yc
 	//    = -[R , 0; dxR , R] [ y, 0]
 	//    = [-Ry ; -dxRy]
-	virtual void impulseInit(const deVector3& point, const deVector3& impulse) = 0;
+	virtual void impulseInit(const deVector3& point, const deVector3& impulse) {}
 
 
 	// Ii = Xc Ic Xtc
 	//    = [ RMRt, -RMRt rx; rx RMRt, RIRt - rx RMRt rx]
 	//    = [ M, -Mrx; rxM, RIRt - m rx rx]
-	virtual void setInertia(const deFloat* mass, const deVector3* centerOfMass, const deMatrix3* inertiaTensor) = 0;
+	virtual void setInertia(const deFloat* mass, const deVector3* centerOfMass, const deMatrix3* inertiaTensor) {}
 
 	// Pi = Xc (Wc X Ic Vc) - Ii (Wi X Vi)  
 	//    = Xc (Wc X [mi vc; Ic wc]) -Ii (Wi X Vi)
-	virtual void biasForce(deVector6& P, const deVector6& V, const deVector3& WxV) = 0;
+	virtual void biasForce(deVector6& P, const deVector6& V, const deVector3& WxV) {}
 
 	// Dinv = inv(St Ia S)
 	// SbarT = Ia S Dinv
 	// hLi = hXi [ 1 - Si Sbari ]^T = hXi [1 - Sbari^T Si^T] = X - X SbarT St
 	// Lt = [1 - S Sbar] Xt
 	// Iah = Ih + sum [ Li Iai Lti ]
-	virtual void _abInertia(deMatrix6& Iah, const deMatrix6& L, const deMatrix6& Ia, const deTransform& X) = 0;
+	virtual void _abInertia(deMatrix6& Iah, const deMatrix6& L, const deMatrix6& Ia, const deTransform& X) {}
 
 	// Pah = Ph - Fexth + sum [ Li (Iai Ci + Pai) + X SbarTi taui ]
-	virtual void _abBiasForce(deVector6& Pah, const deMatrix6& L, const deMatrix6& Ia, const deVector6& C, const deVector6& Pa) = 0;
+	virtual void _abBiasForce(deVector6& Pah, const deMatrix6& L, const deMatrix6& Ia, const deVector6& C, const deVector6& Pa) {}
 
-	virtual void netForce(deVector6& F, const deVector6& A, const deVector6& P) = 0;
+	virtual void netForce(deVector6& F, const deVector6& A, const deVector6& P) {}
+
 	// Pa -= Fext
-	virtual void externalForce(deVector6& Pa, const deVector6& G, const deVector6& Fext) = 0;
+	virtual void externalForce(deVector6& Pa, const deVector6& G, const deVector6& Fext) {}
 
-	virtual void updateLocalX(const deFrame& homeFrame, const deFrame& localFrame) = 0;
+	virtual void updateLocalX(const deFrame& homeFrame, const deFrame& localFrame) {}
 
-	virtual void getFrameLocal(deFrame& localFrame) = 0;
+	virtual void getFrameLocal(deFrame& localFrame) {}
 
-	virtual void abImpulse(deVector6& Yah, deInt propagate) = 0;
+	virtual void abImpulse(deVector6& Yah, deInt propagate) {}
 	// 0Je = Je = iXe^T Si = 0Xi^(-T) Si
 	// where 0Xi^(-T) = [ R rxR; 0 R ]
 	// since 0Re = identity
 	// and  iXe^T = [(inv 0Xi) 0Xe]^T = (0Xe)^T * (0Xi)^(-T) = 0Xi ^(-T)
 	// since  0Xe = identity matrix    <--  {e} = {0}
-	virtual void globalJacobian(const deFrame& globalFrame) = 0;
+	virtual void globalJacobian(const deFrame& globalFrame) {}
 	// A += J * ddQ
-	virtual void plusEq_Jg_ddQ(deVector6& Ag) = 0;
+	virtual void plusEq_Jg_ddQ(deVector6& Ag) {}
 	// Tau += Jt * F
-	virtual void add2Tau_JgT_F(const deVector6& Fg) = 0;
+	virtual void add2Tau_JgT_F(const deVector6& Fg) {}
 	// Gi = Xc Gc
 	// Gc = [ mi Rtci gi ; 0]
 	// gi = Rt gh
@@ -115,7 +116,7 @@ public:
 	//     = [ wxv ; 0 ]
 	// Xt * WxV = [ Rt -Rtdx; 0 Rt ] [ wxv ; 0 ] = [ Rt WxV ; 0 ]
 	virtual void velocity(deVector6& V, deVector3& WxV, const deVector6& Vh, const deVector3& WhxVh) = 0;
-	virtual void velocityOnly(deVector6& V, const deVector6& Vh) = 0;
+	virtual void velocityOnly(deVector6& V, const deVector6& Vh) {}
 	// Hi = hXi^T Hh + Ci
 	virtual void biasAcceleration(deVector6& H, const deVector6& Hh) = 0;
 	// Vi = hXi^T Vh + Si dqi;
@@ -127,27 +128,28 @@ public:
 
 	// ddQ = Dinv*(tau - St*Pa) - Sbar*(X Ah + Ci)
 	// Ai = (hXi^T Ah + Ci) + Si ddqi;
-	virtual void acceleration(deVector6& A, const deVector6& Ah) = 0;
-	virtual void accelerationOnly(deVector6& A, const deVector6& Ah) = 0;
+	virtual void acceleration(deVector6& A, const deVector6& Ah) {}
+	virtual void accelerationOnly(deVector6& A, const deVector6& Ah) {}
+
 	// ddQ = Dinv*(tau - St*Pa) - Sbar*(X Ah)
 	// Ai = (hXi^T Ah) + Si ddqi;
 	// d dQ = Dinv*(- St*Pa) - Sbar*(X dVh)
 	// dVi = (hXi^T dVh) + Si d dqi;
 	virtual void velocityDelta(deVector6& dV, const deVector6& dVh, const deInt dist) = 0;
 
-	virtual void force(deVector6& Fh, deInt propagate) = 0;
+	virtual void force(deVector6& Fh, deInt propagate) {}
 
 	// C = Ph = 0
 	// Pah = - Fexth + sum [ Li (Pai) + X SbarTi taui ]
-	virtual void abBiasForceConfig(deVector6& Pah, deInt propagate) = 0;
-	virtual void abInertiaDepend(deMatrix6& Iah, deVector6& Pah, deMatrix6& Ia, deInt propagate) = 0;
+	virtual void abBiasForceConfig(deVector6& Pah, deInt propagate) {}
+	virtual void abInertiaDepend(deMatrix6& Iah, deVector6& Pah, deMatrix6& Ia, deInt propagate) {}
 	// O = S Dinv St + Lt Oh L
-	virtual void osInertiaInv(deMatrix6& Oa, const deMatrix6& Oah) = 0;
+	virtual void osInertiaInv(deMatrix6& Oa, const deMatrix6& Oah) {}
 
-	virtual void setABJoint(taoABJoint* joint, deInt i = 0) = 0;
+	virtual void setABJoint(taoABJoint* joint, deInt i = 0) {}
 	virtual taoABJoint* getABJoint(deInt i = 0) = 0;
 
-	virtual void setNOJ(deInt n) = 0;
+	virtual void setNOJ(deInt n) {}
 	virtual const deInt getNOJ() const = 0;
 
 private:
@@ -166,44 +168,17 @@ public:
 	virtual deVector6* Pa() { return NULL; }
 	virtual const deMatrix6* I() const { return NULL; }
 
-	virtual void abInertiaInit(deMatrix6& Ia) {}
-	virtual void impulseInit(const deVector3& point, const deVector3& impulse) {}
-
-	virtual void setInertia(const deFloat* mass, const deVector3* centerOfMass, const deMatrix3* inertiaTensor) {}
-	virtual void biasForce(deVector6& P, const deVector6& V, const deVector3& WxV) {}
-	virtual void _abInertia(deMatrix6& Iah, const deMatrix6& L, const deMatrix6& Ia, const deTransform& X) {}
-	virtual void _abBiasForce(deVector6& Pah, const deMatrix6& L, const deMatrix6& Ia, const deVector6& C, const deVector6& Pa) {}
-	virtual void netForce(deVector6& F, const deVector6& A, const deVector6& P) {}
-	virtual void externalForce(deVector6& Pa, const deVector6& G, const deVector6& Fext) {}
-
-	virtual void updateLocalX(const deFrame& homeFrame, const deFrame& localFrame) {}
-	virtual void getFrameLocal(deFrame& localFrame) {}
-
-	virtual void abImpulse(deVector6& Yah, deInt propagate) {}
-	virtual void globalJacobian(const deFrame& globalFrame) {}
-	virtual void plusEq_Jg_ddQ(deVector6& Ag) {}
-	virtual void add2Tau_JgT_F(const deVector6& Fg) {}
-
 	virtual void gravityForce(deVector6& Fext, deVector3& g, const deVector3& gh) { g = gh; }
 	virtual void velocity(deVector6& V, deVector3& WxV, const deVector6& Vh, const deVector3& WhxVh) { WxV.crossMultiply(V[1], V[0]); }
-	virtual void velocityOnly(deVector6& V, const deVector6& Vh) {}
+
 	virtual void biasAcceleration(deVector6& H, const deVector6& Hh) { H.zero(); }
 	virtual deFloat kineticEnergy(deVector6& V, const deVector6& Vh) { return 0; }
 	virtual deFloat potentialEnergy(const deVector3& gh, const deFrame& globalFrame, const deFloat mass, const deVector3& centerOfMass) { return 0; }
-	virtual void acceleration(deVector6& A, const deVector6& Ah) {}
-	virtual void accelerationOnly(deVector6& A, const deVector6& Ah) {}
+
 	virtual void velocityDelta(deVector6& dV, const deVector6& dVh, const deInt dist) { dV.zero(); }
 
-	virtual void force(deVector6& Fh, deInt propagate) {}
-
-	virtual void abBiasForceConfig(deVector6& Pah, deInt propagate) {}
-	virtual void abInertiaDepend(deMatrix6& Iah, deVector6& Pah, deMatrix6& Ia, deInt propagate) {}
-	virtual void osInertiaInv(deMatrix6& Oa, const deMatrix6& Oah) {}
-
-	virtual void setABJoint(taoABJoint* joint, deInt i = 0) {}
 	virtual taoABJoint* getABJoint(deInt i = 0) { return NULL; }
 
-	virtual void setNOJ(deInt n) {}
 	virtual const deInt getNOJ() const { return 0; }
 };
 
