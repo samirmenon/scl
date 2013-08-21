efb78e7861c24b363cb64356cc6584afb9603248
Samir <smenon@stanford.edu>
2013-08-21 01:15:20 -0700

    Removing jcp

--- src/scl/dynamics/tao/tao/dynamics/taoJoint.h
+++ src/scl/dynamics/tao/tao/dynamics/taoJoint.h
@@ -42,7 +42,7 @@
 class taoJoint : public taoDJoint
 {
 public:
-	taoJoint() : _type(TAO_JOINT_USER), _jcp(NULL), _clamp_dQ(0), _max_dQ(5), _damping(0), _inertia(0), _var(NULL), _next(NULL) {}
+	taoJoint() : _type(TAO_JOINT_USER), _clamp_dQ(0), _max_dQ(5), _damping(0), _inertia(0), _var(NULL), _next(NULL) {}
 	virtual ~taoJoint();
 
 	virtual deInt getDOF() = 0;
