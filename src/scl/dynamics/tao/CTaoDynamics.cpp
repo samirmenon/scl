401411e4e54ae1ece6022dfe097035684853efa1
Samir <smenon@stanford.edu>
2013-10-06 16:34:46 -0700

    More refactoring, indentation and typedef removal

--- src/scl/dynamics/tao/CTaoDynamics.cpp
+++ src/scl/dynamics/tao/CTaoDynamics.cpp
@@ -51,30 +51,24 @@ extern "C" {
 
 namespace scl
 {
-
-  CTaoDynamics::
-  CTaoDynamics()
+  CTaoDynamics::CTaoDynamics()
   : robot_name_("(no robot yet)"),
+    tao_tree_q_root_(NULL), tao_tree_q_dq_root_(NULL),
     model_(S_NULL),
     ndof_(0)
   {
   }
 
-
-  CTaoDynamics::
-  ~CTaoDynamics()
+  CTaoDynamics::~CTaoDynamics()
   {
     if(S_NULL!=model_)
     { delete model_;  }
   }
 
-
-  typedef std::map<int, int> id_counter_t;
-
-  static void tao_collect_ids(taoDNode * node, id_counter_t & id_counter)
+  static void tao_collect_ids(taoDNode * node, std::map<int, int> & id_counter)
   {
     int const id(node->getID());
-    id_counter_t::iterator idc(id_counter.find(id));
+    std::map<int, int>::iterator idc(id_counter.find(id));
     if (id_counter.end() == idc) {
       id_counter.insert(std::make_pair(id, 1));
     }
@@ -93,13 +87,13 @@ namespace scl
       std::cout << "scl::tao_consistency_check(): root has ID " << root->getID() << " instead of -1\n";
       return false;
     }
-    id_counter_t id_counter;
+    std::map<int, int> id_counter;
     for (taoDNode * node(root->getDChild()); 0 != node; node = node->getDSibling()) {
       tao_collect_ids(node, id_counter);
     }
     int expected_id(0);
     //NOTE TODO: XXXX  : Is this really necessary?
-    for (id_counter_t::const_iterator idc(id_counter.begin()); idc != id_counter.end(); ++idc, ++expected_id) {
+    for (std::map<int, int>::const_iterator idc(id_counter.begin()); idc != id_counter.end(); ++idc, ++expected_id) {
       if (idc->first != expected_id) {
         std::cout << "scl::tao_consistency_check(): ID gap, expected "
             << expected_id << " but encountered " << idc->first << "\n";
@@ -117,84 +111,91 @@ namespace scl
   bool CTaoDynamics::
   init(const SRobotParsedData& arg_robot_data)
   {
-    if (model_) {
-      fprintf(stderr, "scl::CTaoDynamics::init(): already initialized\n");
-      return false;
-    }
-
-    if(false == arg_robot_data.has_been_init_){
-      fprintf(stderr, "scl::CTaoDynamics::init(): Passed an uninitialized robot\n");
-      return false;
-    }
-
-    //Set all the robot parameters
-    robot_name_ = arg_robot_data.name_;
-    gravity_ = arg_robot_data.gravity_;
-
-    //Initialize the tao tree
-    tao_tree_q_root_ = scl::CTaoRepCreator::taoRootRepCreator(arg_robot_data);
-    if ( ! tao_tree_q_root_) {
-      std::cout << "scl::CTaoDynamics::init(`" << robot_name_
-          << "'): scl::CTaoRepCreator::taoRootRepCreator() failed [invalid robot name?]\n";
-      return false;
-    }
-
-    if ( ! tao_consistency_check(tao_tree_q_root_)) {
-      std::cout << "scl::CTaoDynamics::init(`" << robot_name_
-          << "'): consistency check failed on TAO tree\n";
-      return false;
-    }
-
-    // Parse it again to get a second copy. Extra cost only incurred
-    // at init time, so that should be acceptable, although it would
-    // be cleaner to have TAO tree deep copy functionality somewhere.
-    tao_tree_q_dq_root_ = scl::CTaoRepCreator::taoRootRepCreator(arg_robot_data);
-    if ( ! tao_tree_q_dq_root_) {
-      std::cout << "scl::CTaoDynamics::init(`" << robot_name_
-          << "'): scl::CTaoRepCreator::taoRootRepCreator(cc) failed [invalid robot name?]\n";
-      return false;
-    }
-
-    jspace::STaoTreeInfo * js_q_tree(jspace::create_bare_tao_tree_info(tao_tree_q_root_));
-    jspace::STaoTreeInfo * js_q_dq_tree(jspace::create_bare_tao_tree_info(tao_tree_q_dq_root_));
-
-    //NOTE TODO Perhaps this was what TRY_TO_CONVERT_NAMES achieved
-    sutil::CMappedTree<std::basic_string<char>, scl::SRigidBody>::const_iterator itbr, itbre;
-    for(itbr = arg_robot_data.robot_br_rep_.begin(),
-        itbre = arg_robot_data.robot_br_rep_.end();
-        itbr!=itbre; ++itbr)
+    try
     {
-      std::vector<jspace::STaoNodeInfo>::iterator it, ite, icc;
+      has_been_init_ = false;
+      if (model_) {
+        fprintf(stderr, "scl::CTaoDynamics::init(): already initialized\n");
+        return false;
+      }
 
-      //Name the stuff in the kgm tree
-      it = js_q_tree->info.begin();
-      icc = js_q_dq_tree->info.begin();
-      ite = js_q_tree->info.end();
-      for (/**/; it != ite; ++it, ++icc)
+      if(false == arg_robot_data.has_been_init_){
+        fprintf(stderr, "scl::CTaoDynamics::init(): Passed an uninitialized robot\n");
+        return false;
+      }
+
+      //Set all the robot parameters
+      robot_name_ = arg_robot_data.name_;
+      gravity_ = arg_robot_data.gravity_;
+
+      //Initialize the tao tree
+      tao_tree_q_root_ = scl::CTaoRepCreator::taoRootRepCreator(arg_robot_data);
+      if ( ! tao_tree_q_root_) {
+        std::cout << "scl::CTaoDynamics::init(`" << robot_name_
+            << "'): scl::CTaoRepCreator::taoRootRepCreator() failed [invalid robot name?]\n";
+        return false;
+      }
+
+      if ( ! tao_consistency_check(tao_tree_q_root_)) {
+        std::cout << "scl::CTaoDynamics::init(`" << robot_name_
+            << "'): consistency check failed on TAO tree\n";
+        return false;
+      }
+
+      // Parse it again to get a second copy. Extra cost only incurred
+      // at init time, so that should be acceptable, although it would
+      // be cleaner to have TAO tree deep copy functionality somewhere.
+      tao_tree_q_dq_root_ = scl::CTaoRepCreator::taoRootRepCreator(arg_robot_data);
+      if ( ! tao_tree_q_dq_root_) {
+        std::cout << "scl::CTaoDynamics::init(`" << robot_name_
+            << "'): scl::CTaoRepCreator::taoRootRepCreator(cc) failed [invalid robot name?]\n";
+        return false;
+      }
+
+      jspace::STaoTreeInfo * js_q_tree(jspace::create_bare_tao_tree_info(tao_tree_q_root_));
+      jspace::STaoTreeInfo * js_q_dq_tree(jspace::create_bare_tao_tree_info(tao_tree_q_dq_root_));
+
+      //NOTE TODO Perhaps this was what TRY_TO_CONVERT_NAMES achieved
+      sutil::CMappedTree<std::basic_string<char>, scl::SRigidBody>::const_iterator itbr, itbre;
+      for(itbr = arg_robot_data.robot_br_rep_.begin(),
+          itbre = arg_robot_data.robot_br_rep_.end();
+          itbr!=itbre; ++itbr)
       {
-        const SRigidBody& l_ds = *itbr;
-        if (l_ds.name_ == it->node->name_)
+        std::vector<jspace::STaoNodeInfo>::iterator it, ite, icc;
+
+        //Name the stuff in the kgm tree
+        it = js_q_tree->info.begin();
+        icc = js_q_dq_tree->info.begin();
+        ite = js_q_tree->info.end();
+        for (/**/; it != ite; ++it, ++icc)
         {
-          it->link_name   = itbr->name_;
-          it->joint_name  = itbr->joint_name_;
-          it->limit_lower = itbr->joint_limit_lower_;
-          it->limit_upper = itbr->joint_limit_upper_;
-          icc->link_name  = itbr->name_;
-          icc->joint_name = itbr->joint_name_;
-          icc->limit_lower = itbr->joint_limit_lower_;
-          icc->limit_upper = itbr->joint_limit_upper_;
-          break;
+          const SRigidBody& l_ds = *itbr;
+          if (l_ds.name_ == it->node->name_)
+          {
+            it->link_name   = itbr->name_;
+            it->joint_name  = itbr->joint_name_;
+            it->limit_lower = itbr->joint_limit_lower_;
+            it->limit_upper = itbr->joint_limit_upper_;
+            icc->link_name  = itbr->name_;
+            icc->joint_name = itbr->joint_name_;
+            icc->limit_lower = itbr->joint_limit_lower_;
+            icc->limit_upper = itbr->joint_limit_upper_;
+            break;
+          }
         }
       }
+
+      model_ = new jspace::Model();
+      model_->init(js_q_tree, js_q_dq_tree, gravity_(0), gravity_(1), gravity_(2), 0);
+      ndof_ = model_->getNDOF();
+      state_.init(ndof_, ndof_, 0);
+
+      has_been_init_ = true;
     }
+    catch(std::exception& e)
+    { std::cerr<<"\nCTaoDynamics::init() : Error : "<<e.what();  }
 
-    model_ = new jspace::Model();
-    model_->init(js_q_tree, js_q_dq_tree, gravity_(0), gravity_(1), gravity_(2), 0);
-    ndof_ = model_->getNDOF();
-    state_.init(ndof_, ndof_, 0);
-
-    has_been_init_ = true;
-    return true;
+    return has_been_init_;
   }
 
 
