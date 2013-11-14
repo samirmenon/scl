dc8674416b96850433841eaf6e7e65b4d434783b
Samir <smenon@stanford.edu>
2013-11-14 13:31:37 -0800

    Removing tao junk comments

--- src/scl/dynamics/tao/CTaoDynamics.hpp
+++ src/scl/dynamics/tao/CTaoDynamics.hpp
@@ -81,41 +81,6 @@ namespace scl
      */
     taoDNode * getTaoIdForLink(std::string arg_link_name);
 
-//    /** Calculates the Transformation Matrix for the robot to which
-//     * this dynamics object is assigned.
-//     *
-//     * The Transformation Matrix is specified by a link and an offset
-//     * (in task space dimensions)from that link and is given by:
-//     *
-//     *           x_global_coords = T * x_link_coords
-//     *
-//     * Uses id based link lookup. The dynamics implementation should
-//     * support this (maintain a map or something).
-//     */
-//    virtual sBool computeTransform_Depracated(
-//        /** The link at which the transformation matrix is
-//         * to be calculated */
-//        const void* arg_link_id,
-//        /** The transformation matrix will be saved here. */
-//        Eigen::Affine3d& arg_T);
-
-//    /** Calculates the Jacobian for the robot to which this dynamics
-//     * object is assigned.
-//     *
-//     * The Jacobian is specified by a link and an offset (in task space
-//     * dimensions)from that link
-//     *
-//     * Uses id based link lookup. The dynamics implementation should
-//     * support this (maintain a map or something).
-//     */
-//    virtual sBool computeJacobian_Depracated(
-//        /** The link at which the Jacobian is to be calculated */
-//        const void* arg_link_id,
-//        /** The offset from the link's frame. */
-//        const Eigen::VectorXd& arg_pos_global,
-//        /** The Jacobain will be saved here. */
-//        Eigen::MatrixXd& arg_J);
-
     /** Integrates the robot's state.
      * Uses the given applied forces, torques, positions and velocities
      * and its internal dynamic model to compute
@@ -148,12 +113,6 @@ namespace scl
     /* *******************************************************************
      *                      Dynamics State functions.
      * ******************************************************************* */
-//    /** Gets the robot's kinetic energy */
-//    virtual sFloat getKineticEnergy_Depracated();
-
-//    /** Gets the robot's potential energy */
-//    virtual sFloat getPotentialEnergy_Depracated();
-
     /** Sets the generalized coordinates
      *
      * TODO : This dynamics engine implementation should be stateless.
