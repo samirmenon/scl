06fde9ff9303f890a80d84702e407a2827a63d00
Samir <smenon@stanford.edu>
2014-08-02 23:47:25 -0700

    Removing unused db spec

--- src/scl/graphics/chai/ChaiGlutHandlers.cpp
+++ src/scl/graphics/chai/ChaiGlutHandlers.cpp
@@ -400,11 +400,6 @@ namespace scl_chai_glut_interface
   //---------------------------------------------------------------------------
   void updateGraphics(void)
   {
-    scl::SDatabase * db = scl::CDatabase::getData();
-#ifdef DEBUG
-    if(NULL == db) {std::cerr<<"\nupdateGraphics() : Database not initialized"; }
-#endif
-
     SChaiGlobals* chai_glob_ds = CChaiGlobals::getData();
     //Update the IO
     keyHandler();
