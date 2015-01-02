00e8836be1ec6b9966db11cbfc77a48c7babf731
Samir <smenon@stanford.edu>
2015-01-02 03:07:49 -0800

    Using macros to avoid rewriting tedious code.

--- src/scl/serialization/SerializationJSON.cpp
+++ src/scl/serialization/SerializationJSON.cpp
@@ -30,9 +30,34 @@ scl. If not, see <http://www.gnu.org/licenses/>.
  */
 #include <scl/serialization/SerializationJSON.hpp>
 #include <scl/util/EigenExtensions.hpp>
+#include <cmath>
 
 namespace scl
 {
+  /** *****************************************************************************
+   *                  Terrible macros to greatly simplify life
+   * **************************************************************************** */
+  //Yes this is terrible. But seriously. Who the heck wants to write tons of useless code.
+  //This is where a metacompiler would be useful. moc moc moc.. Sigh...
+  //NOTE TODO : Remove this when you write a script to autogen all this stuff...
+
+#define MACRO_SER_ARGOBJ_RETJSONVAL(AAA) ret_json_val[#AAA] = arg_obj.AAA;
+
+#define MACRO_SER_ARGOBJ_RETJSONVAL_Eigen(AAA) \
+  scl_util::eigentoStringArrayJSON(arg_obj.AAA, str); \
+  json_reader.parse(str, ret_json_val[#AAA]);
+
+
+#define MACRO_DESER_RETOBJ_ARGJSONVAL(AAA,TTT) \
+    if(!arg_json_val.isMember(#AAA)) { \
+      std::cout<<"\n deserializeFromJSON() Error : Could not find : "<<#AAA<<std::flush; \
+      return false; \
+    } \
+    ret_obj.AAA = arg_json_val[#AAA].TTT();
+
+  //NOTE TODO : Finish this..
+#define MACRO_DESER_RETOBJ_ARGJSONVAL_Eigen(AAA)
+
   /** *****************************************************************************
    *                          Serialize data to a string
    * **************************************************************************** */
@@ -56,9 +81,9 @@ namespace scl
 
   template<> bool serializeToJSON<SObject>(const SObject &arg_obj, Json::Value &ret_json_val)
   {
-    ret_json_val["has_been_init"] = arg_obj.hasBeenInit();
-    ret_json_val["name"] = arg_obj.getName();
-    ret_json_val["type"] = arg_obj.getType();
+    ret_json_val["has_been_init_"] = arg_obj.hasBeenInit();
+    ret_json_val["name_"] = arg_obj.getName();
+    ret_json_val["type_"] = arg_obj.getType();
     return true;
   }
 
@@ -70,35 +95,34 @@ namespace scl
     Json::Reader json_reader;
     std::string str;
 
-    ret_json_val["collision_type"] = arg_obj.collision_type_;
-    scl_util::eigentoStringArrayJSON(arg_obj.com_, str);
-    json_reader.parse(str, ret_json_val["com"]);
-    ret_json_val["force_gc_lim_lower"] = arg_obj.force_gc_lim_lower_;
-    ret_json_val["force_gc_lim_upper"] = arg_obj.force_gc_lim_upper_;
-    ret_json_val["friction_gc_kv"] = arg_obj.friction_gc_kv_;
-    scl_util::eigentoStringArrayJSON(arg_obj.inertia_, str);
-    json_reader.parse(str, ret_json_val["inertia"]);
-    ret_json_val["inertia_gc"] = arg_obj.inertia_gc_;
-    ret_json_val["is_root"] = arg_obj.is_root_;
-    ret_json_val["joint_default_pos"] = arg_obj.joint_default_pos_;
-    ret_json_val["joint_limit_lower"] = arg_obj.joint_limit_lower_;
-    ret_json_val["joint_limit_upper"] = arg_obj.joint_limit_upper_;
-    ret_json_val["joint_name"] = arg_obj.joint_name_;
-    ret_json_val["joint_type"] = arg_obj.joint_type_;
-    ret_json_val["link_id"] = arg_obj.link_id_;
-    ret_json_val["link_is_fixed"] = arg_obj.link_is_fixed_;
-    ret_json_val["mass"] = arg_obj.mass_;
-    scl_util::eigentoStringArrayJSON(arg_obj.ori_parent_quat_, str);
-    json_reader.parse(str, ret_json_val["ori_parent_quat"]);
-    ret_json_val["parent_name"] = arg_obj.parent_name_;
-    scl_util::eigentoStringArrayJSON(arg_obj.pos_in_parent_, str);
-    json_reader.parse(str, ret_json_val["pos_in_parent"]);
-    ret_json_val["render_type"] = arg_obj.render_type_;
-    ret_json_val["robot_name"] = arg_obj.robot_name_;
-    ret_json_val["stiction_gc_force_lower"] = arg_obj.stiction_gc_force_lower_;
-    ret_json_val["stiction_gc_force_upper"] = arg_obj.stiction_gc_force_upper_;
-    ret_json_val["stiction_gc_vel_lower"] = arg_obj.stiction_gc_vel_lower_;
-    ret_json_val["stiction_gc_vel_upper"] = arg_obj.stiction_gc_vel_upper_;
+    //Read in the standard types (supported by json)
+    MACRO_SER_ARGOBJ_RETJSONVAL(collision_type_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(force_gc_lim_lower_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(force_gc_lim_upper_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(friction_gc_kv_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(inertia_gc_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(is_root_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(joint_default_pos_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(joint_limit_lower_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(joint_limit_upper_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(joint_name_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(joint_type_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(link_id_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(link_is_fixed_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(mass_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(parent_name_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(render_type_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(robot_name_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(stiction_gc_force_lower_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(stiction_gc_force_upper_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(stiction_gc_vel_lower_);
+    MACRO_SER_ARGOBJ_RETJSONVAL(stiction_gc_vel_upper_);
+
+    //Read in the Eigen matrix types..
+    MACRO_SER_ARGOBJ_RETJSONVAL_Eigen(com_);
+    MACRO_SER_ARGOBJ_RETJSONVAL_Eigen(inertia_);
+    MACRO_SER_ARGOBJ_RETJSONVAL_Eigen(ori_parent_quat_);
+    MACRO_SER_ARGOBJ_RETJSONVAL_Eigen(pos_in_parent_);
 
     return flag;
   }
@@ -138,48 +162,45 @@ namespace scl
 
   template<> bool deserializeFromJSON<SObject>(SObject &ret_obj, const Json::Value &arg_json_val)
   {
-    ret_obj.has_been_init_ = arg_json_val.get("has_been_init",false).asBool();
-    ret_obj.name_ = arg_json_val.get("name","").asString();
-    // Type is hard-wired by the associated subclass..
+    MACRO_DESER_RETOBJ_ARGJSONVAL(has_been_init_,asBool);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(name_,asString);
     return true;
   }
 
   template<> bool deserializeFromJSON<SRigidBody>(SRigidBody &ret_obj, const Json::Value &arg_json_val)
   {
     bool flag = deserializeFromJSON(*dynamic_cast<const SObject*>(&ret_obj), arg_json_val);
-    if(!flag) { return false; }
+    if(!flag){ return false; }
 
-//    arg_json_val["collision_type"] = ret_obj.collision_type_;
-//    scl_util::eigentoStringArrayJSON(ret_obj.com_, str);
-//    json_reader.parse(str, arg_json_val["com"]);
-//    arg_json_val["force_gc_lim_lower"] = ret_obj.force_gc_lim_lower_;
-//    arg_json_val["force_gc_lim_upper"] = ret_obj.force_gc_lim_upper_;
-//    arg_json_val["friction_gc_kv"] = ret_obj.friction_gc_kv_;
-//    scl_util::eigentoStringArrayJSON(ret_obj.inertia_, str);
-//    json_reader.parse(str, arg_json_val["inertia"]);
-//    arg_json_val["inertia_gc"] = ret_obj.inertia_gc_;
-//    arg_json_val["is_root"] = ret_obj.is_root_;
-//    arg_json_val["joint_default_pos"] = ret_obj.joint_default_pos_;
-//    arg_json_val["joint_limit_lower"] = ret_obj.joint_limit_lower_;
-//    arg_json_val["joint_limit_upper"] = ret_obj.joint_limit_upper_;
-//    arg_json_val["joint_name"] = ret_obj.joint_name_;
-//    arg_json_val["joint_type"] = ret_obj.joint_type_;
-//    arg_json_val["link_id"] = ret_obj.link_id_;
-//    arg_json_val["link_is_fixed"] = ret_obj.link_is_fixed_;
-//    arg_json_val["mass"] = ret_obj.mass_;
-//    scl_util::eigentoStringArrayJSON(ret_obj.ori_parent_quat_, str);
-//    json_reader.parse(str, arg_json_val["ori_parent_quat"]);
-//    arg_json_val["parent_name"] = ret_obj.parent_name_;
-//    scl_util::eigentoStringArrayJSON(ret_obj.pos_in_parent_, str);
-//    json_reader.parse(str, arg_json_val["pos_in_parent"]);
-//    arg_json_val["render_type"] = ret_obj.render_type_;
-//    arg_json_val["robot_name"] = ret_obj.robot_name_;
-//    arg_json_val["stiction_gc_force_lower"] = ret_obj.stiction_gc_force_lower_;
-//    arg_json_val["stiction_gc_force_upper"] = ret_obj.stiction_gc_force_upper_;
-//    arg_json_val["stiction_gc_vel_lower"] = ret_obj.stiction_gc_vel_lower_;
-//    arg_json_val["stiction_gc_vel_upper"] = ret_obj.stiction_gc_vel_upper_;
+    //Read in the standard types (supported by json)
+    MACRO_DESER_RETOBJ_ARGJSONVAL(collision_type_,asInt);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(force_gc_lim_lower_,asDouble);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(force_gc_lim_upper_,asDouble);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(friction_gc_kv_,asDouble);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(inertia_gc_,asDouble);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(is_root_,asBool);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(joint_default_pos_,asDouble);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(joint_limit_lower_,asDouble);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(joint_limit_upper_,asDouble);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(joint_name_,asString);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(link_id_,asInt);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(link_is_fixed_,asBool);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(mass_,asDouble);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(parent_name_,asString);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(robot_name_,asString);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(stiction_gc_force_lower_,asDouble);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(stiction_gc_force_upper_,asDouble);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(stiction_gc_vel_lower_,asDouble);
+    MACRO_DESER_RETOBJ_ARGJSONVAL(stiction_gc_vel_upper_,asDouble);
 
-    return flag;
+    //Special cases. Too much work to make a macro..
+    if(!arg_json_val.isMember("joint_type_")) return false;
+    ret_obj.joint_type_ = static_cast<EJointType> (arg_json_val["joint_type"].asInt());
+
+    if(!arg_json_val.isMember("render_type_")) return false;
+    ret_obj.render_type_ = static_cast<ERenderType> (arg_json_val["render_type"].asInt());
+
+    return true;
   }
 
   template<> bool deserializeFromJSON<SRigidBodyDyn>(SRigidBodyDyn &ret_obj, const Json::Value &arg_json_val)
