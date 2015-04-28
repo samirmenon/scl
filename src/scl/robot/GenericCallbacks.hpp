9d45ae227c1b5a0291c77f944cc43c4b0e4333a9
Samir <smenon@stanford.edu>
2015-04-28 03:10:57 -0700

    Fixing some indentation

--- src/scl/robot/GenericCallbacks.hpp
+++ src/scl/robot/GenericCallbacks.hpp
@@ -49,23 +49,24 @@ namespace scl
   /** An echo function call. Echoes a string back on to the console.
    * (An example for you) */
   class CCallbackEcho : public sutil::CCallbackBase<std::string, std::vector<std::string> >
-  { typedef sutil::CCallbackBase<std::string, std::vector<std::string> > base;
+  {
+    typedef sutil::CCallbackBase<std::string, std::vector<std::string> > base;
   public:
-  virtual void call(std::vector<std::string>& arg)
-  {//Print out all the arguments (except the first, which is the command)
-    if(1 >= arg.size()) {}
-    else if("--help" == arg[1])
-    { std::cout<<" >>echo\n Echoes a string back on to the screen"; }
-    else
-    {
-      std::vector<std::string>::iterator it, ite;
-      for(it = arg.begin()+1, ite = arg.end(); it != ite; ++it)
-      { std::cout<<*it<<" "; }
+    virtual void call(std::vector<std::string>& arg)
+    {//Print out all the arguments (except the first, which is the command)
+      if(1 >= arg.size()) {}
+      else if("--help" == arg[1])
+      { std::cout<<" >>echo\n Echoes a string back on to the screen"; }
+      else
+      {
+        std::vector<std::string>::iterator it, ite;
+        for(it = arg.begin()+1, ite = arg.end(); it != ite; ++it)
+        { std::cout<<*it<<" "; }
+      }
     }
-  }
 
-  virtual base* createObject()
-  { return dynamic_cast<base*>(new CCallbackEcho()); }
+    virtual base* createObject()
+    { return dynamic_cast<base*>(new CCallbackEcho()); }
   };
 
   /** Print out a specific data structure.
@@ -75,30 +76,31 @@ namespace scl
    * with sutil::printables. (Open the file to see an
    * example of how)*/
   class CCallbackPrint : public sutil::CCallbackBase<std::string, std::vector<std::string> >
-  { typedef sutil::CCallbackBase<std::string, std::vector<std::string> > base;
+  {
+    typedef sutil::CCallbackBase<std::string, std::vector<std::string> > base;
   public:
-  virtual void call(std::vector<std::string>& arg)
-  {//Print out all the arguments (except the first, which is the command)
-    if(1 >= arg.size())
-    { std::cout<<"Object not found";  }
-    else if("--help" == arg[1])
-    { std::cout<<" >>print <object_name>\n Finds the object in the print registry and prints its contents"; }
-    else
-    {//Prints the contents of the object
-      std::vector<std::string>::iterator it, ite;
-      for(it = arg.begin()+1, ite = arg.end(); it != ite; ++it)
-      {
-        const sutil::SPrintableBase* obj = sutil::printables::get(*it);
-        if(NULL == obj)
-        { std::cout<<"Object not found";  }
-        else
-        { std::cout<<(*obj);  }
+    virtual void call(std::vector<std::string>& arg)
+    {//Print out all the arguments (except the first, which is the command)
+      if(1 >= arg.size())
+      { std::cout<<"Object not found";  }
+      else if("--help" == arg[1])
+      { std::cout<<" >>print <object_name>\n Finds the object in the print registry and prints its contents"; }
+      else
+      {//Prints the contents of the object
+        std::vector<std::string>::iterator it, ite;
+        for(it = arg.begin()+1, ite = arg.end(); it != ite; ++it)
+        {
+          const sutil::SPrintableBase* obj = sutil::printables::get(*it);
+          if(NULL == obj)
+          { std::cout<<"Object not found";  }
+          else
+          { std::cout<<(*obj);  }
+        }
       }
     }
-  }
 
-  virtual base* createObject()
-  { return dynamic_cast<base*>(new CCallbackPrint()); }
+    virtual base* createObject()
+    { return dynamic_cast<base*>(new CCallbackPrint()); }
   };
 
 
@@ -109,52 +111,54 @@ namespace scl
    * with scl::CObjectMap. (Open scl/Singletons.hpp to see the
    * class)*/
   class CCallbackSet : public sutil::CCallbackBase<std::string, std::vector<std::string> >
-  { typedef sutil::CCallbackBase<std::string, std::vector<std::string> > base;
+  {
+    typedef sutil::CCallbackBase<std::string, std::vector<std::string> > base;
   public:
-  virtual void call(std::vector<std::string>& arg)
-  {//Set out all the arguments (except the first, which is the command)
-    if(1 >= arg.size())
-    { std::cout<<"Object not found";  }
-    else if("--help" == arg[1])
-    { std::cout<<" >>set <object_name> <arg_str1> <arg_str2> ... <arg_strN>"
-      <<"\n Sets the contents of an object in the database"; }
-    else
-    {//Sets the contents of the object
-//      const scl::SObject** obj = scl::CObjectMap::getData()->at_const(*(arg.begin()+1));
-//      if(NULL == obj) { std::cout<<"Object not found";  }
-//      else
-//      { std::cout<<(*obj);  }
+    virtual void call(std::vector<std::string>& arg)
+    {//Set out all the arguments (except the first, which is the command)
+      if(1 >= arg.size())
+      { std::cout<<"Object not found";  }
+      else if("--help" == arg[1])
+      { std::cout<<" >>set <object_name> <arg_str1> <arg_str2> ... <arg_strN>"
+        <<"\n Sets the contents of an object in the database"; }
+      else
+      {//Sets the contents of the object
+        //const scl::SObject** obj = scl::CObjectMap::getData()->at_const(*(arg.begin()+1));
+        //if(NULL == obj) { std::cout<<"Object not found";  }
+        //else
+        //{ std::cout<<(*obj);  }
+      }
     }
-  }
 
-  virtual base* createObject()
-  { return dynamic_cast<base*>(new CCallbackSet()); }
+    virtual base* createObject()
+    { return dynamic_cast<base*>(new CCallbackSet()); }
   };
 
   /** Prints out a generic help message and also lists all the
    * other available commands */
   class CCallbackHelp : public sutil::CCallbackBase<std::string, std::vector<std::string> >
-  { typedef sutil::CCallbackBase<std::string, std::vector<std::string> > base;
+  {
+    typedef sutil::CCallbackBase<std::string, std::vector<std::string> > base;
   public:
-  virtual void call(std::vector<std::string>& arg)
-  {//Help out all the arguments (except the first, which is the command)
-    std::vector<std::string> idxlist;
-    //Get a list of all the callbacks in the registry
-    if(false == sutil::callbacks::list(idxlist))
-    { std::cout<<"Callback registry is empty";  }
-    else
-    {
-      if(1 >= idxlist.size())
-      { std::cout<<"Available commands:"; }
-      std::vector<std::string>::iterator it,ite;
-      for(it = idxlist.begin(), ite = idxlist.end(); it!=ite; ++it)
-      { std::cout<<" "<<(*it); } //Print all the callbacks
-      std::cout<<"\nFor details, type: <command> --help\nTo switch to char mode, type: x\nTo exit, type: exit";
+    virtual void call(std::vector<std::string>& arg)
+    {//Help out all the arguments (except the first, which is the command)
+      std::vector<std::string> idxlist;
+      //Get a list of all the callbacks in the registry
+      if(false == sutil::callbacks::list(idxlist))
+      { std::cout<<"Callback registry is empty";  }
+      else
+      {
+        if(1 >= idxlist.size())
+        { std::cout<<"Available commands:"; }
+        std::vector<std::string>::iterator it,ite;
+        for(it = idxlist.begin(), ite = idxlist.end(); it!=ite; ++it)
+        { std::cout<<" "<<(*it); } //Print all the callbacks
+        std::cout<<"\nFor details, type: <command> --help\nTo switch to char mode, type: x\nTo exit, type: exit";
+      }
     }
-  }
 
-  virtual base* createObject()
-  { return dynamic_cast<base*>(new CCallbackHelp()); }
+    virtual base* createObject()
+    { return dynamic_cast<base*>(new CCallbackHelp()); }
   };
 
   /** Key hander callbacks for decrementing a double data member
