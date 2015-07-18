025d0508ff661390e84750080cf3850253e431a5
Samir <smenon@stanford.edu>
2015-07-18 06:23:24 -0700

    Updating the graphics ds init

---
+++ src/scl/graphics/chai/data_structs/SGraphicsChai.cpp
@@ -0,0 +1,182 @@
+/* This file is part of scl, a control and simulation library
+for robots and biomechanical models.
+
+scl is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+Alternatively, you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of
+the License, or (at your option) any later version.
+
+scl is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License and a copy of the GNU General Public License along with
+scl. If not, see <http://www.gnu.org/licenses/>.
+ */
+/* \file SGraphicsChai.cpp
+ *
+ *  Created on: Jul 18, 2015
+ *
+ *  Copyright (C) 2015
+ *
+ *  Author: Samir Menon <smenon@stanford.edu>
+ */
+#include "SGraphicsChai.hpp"
+
+#include "chai3d.h"
+
+#include <iostream>
+
+namespace scl
+{
+  bool SGraphicsChai::init(const SGraphicsParsed* arg_gr_ds, const bool arg_reset_chai_world)
+  {
+    bool chai_ds_deleted_ = false;
+    bool flag;
+    try
+    {
+      if(S_NULL == arg_gr_ds)
+      { throw(std::runtime_error("Passed NULL object for the parsed graphics specification.")); }
+
+      // We'll reset the initialization flag right here..
+      has_been_init_ = false;
+
+#ifdef DEBUG
+      if(S_NULL != chai_cam_ || S_NULL != chai_world_)
+      { std::cout<<"\nSGraphicsChai::init() : Chai data structures are already initialized. Option to reset = "<<arg_reset_chai_world;  }
+#endif
+
+      // Decide whether to reset the chai world..
+      if(arg_reset_chai_world)
+      {
+        if(S_NULL!= chai_world_) { delete chai_world_; chai_world_ = S_NULL; }
+        if(S_NULL!= chai_cam_)   { delete chai_cam_; chai_cam_ = S_NULL; }
+      }
+
+      // Reallocate memory if we have the world set to a null pointers by the time we get here.
+      if(S_NULL== chai_world_)
+      {
+        //Create a new world : Don't worry about deallocating it.
+        chai_world_ = new chai3d::cWorld();
+        if(S_NULL == chai_world_) { throw(std::runtime_error("Couldn't initialize chai world"));  }
+
+        // NOTE: If the world is reset, the camera has to be reset..
+        if(S_NULL!= chai_cam_)   { delete chai_cam_; chai_cam_ = S_NULL; }
+      }
+      if(S_NULL == chai_cam_)
+      {
+        //Create a camera
+        chai_cam_ = new chai3d::cCamera(chai_world_);
+        if(S_NULL == chai_cam_) { throw(std::runtime_error("Couldn't create a chai camera")); }
+      }
+
+      //Name it
+      name_ = arg_gr_ds->name_;
+
+      //Set up chai world and camera
+      chai_world_->m_userName = "scl_id_chai_world";
+
+      //Set the background color (R,G,B)
+      chai_world_->setBackgroundColor(arg_gr_ds->background_color_[0],arg_gr_ds->background_color_[1], arg_gr_ds->background_color_[2]);
+
+      //Insert the camera into the scenegraph world
+      chai_world_->addChild(chai_cam_);
+
+      //Set up the camera
+      chai_cam_->m_userName = "scl_id_chai_cam";
+      chai3d::cVector3d tmp1(arg_gr_ds->cam_pos_[0], arg_gr_ds->cam_pos_[1], arg_gr_ds->cam_pos_[2]);
+      chai3d::cVector3d tmp2(arg_gr_ds->cam_lookat_[0], arg_gr_ds->cam_lookat_[1], arg_gr_ds->cam_lookat_[2]);
+      chai3d::cVector3d tmp3(arg_gr_ds->cam_up_[0], arg_gr_ds->cam_up_[1], arg_gr_ds->cam_up_[2]);
+
+      // Set up the camera
+      chai_cam_->set(/** position (eye) */tmp1, /** lookat position (target)*/ tmp2,
+          /** "up" direction */ tmp3);
+
+#ifdef DEBUG
+      std::cout<<"\nCam Pos    :"<<arg_gr_ds->cam_pos_[0]<<" "<<arg_gr_ds->cam_pos_[1]<<" "<<arg_gr_ds->cam_pos_[2];
+      std::cout<<"\nCam Lookat :"<<arg_gr_ds->cam_lookat_[0]<<" "<<arg_gr_ds->cam_lookat_[1]<<" "<< arg_gr_ds->cam_lookat_[2];
+      std::cout<<"\nCam Up     :"<<arg_gr_ds->cam_up_[0]<<" "<<arg_gr_ds->cam_up_[1]<<" "<< arg_gr_ds->cam_up_[2];
+      std::cout<<"\nBack color : "<<arg_gr_ds->background_color_[0]<<", "
+          <<arg_gr_ds->background_color_[1]<<", "<<arg_gr_ds->background_color_[2]<<std::endl;
+#endif
+
+      //Set the near and far clipping planes of the camera
+      //Anything in front/behind these clipping planes will not be rendered
+      chai_cam_->setClippingPlanes(arg_gr_ds->cam_clipping_dist_[0], arg_gr_ds->cam_clipping_dist_[1]);
+
+      //Attach background/foreground images
+      if(arg_gr_ds->file_background_ != "")
+      {
+        chai3d::cBackground *img = new chai3d::cBackground();
+        flag = img->loadFromFile(arg_gr_ds->file_background_);
+        if(false == flag)
+        { std::cout<<"\nSGraphicsChai::init() : ERROR : Could not load background image. Proceeding...";  }
+        else{
+          // Attach the background to the camera's back layer. Chai's default
+          // is to support "widgets" like this on layers (see chai code for more).
+          chai_cam_->m_backLayer->addChild(img);
+        }
+      }
+      //Attach background/foreground images
+      if(arg_gr_ds->file_foreground_ != "")
+      {
+        chai3d::cBackground *img = new chai3d::cBackground();
+        flag = img->loadFromFile(arg_gr_ds->file_foreground_);
+        if(false == flag)
+        { std::cout<<"\nSGraphicsChai::init() : ERROR : Could not load foreground image. Proceeding...";  }
+        else{
+          // Attach the foreground to the camera's back layer. Chai's default
+          // is to support "widgets" like this on layers (see chai code for more).
+          chai_cam_->m_frontLayer->addChild(img);
+        }
+      }
+
+      // create a light source and attach it to the camera
+      std::vector<SGraphicsParsed::SLight>::const_iterator it,ite;
+      for(it = arg_gr_ds->lights_.begin(), ite = arg_gr_ds->lights_.end(); it!=ite; ++it)
+      {
+        chai3d::cDirectionalLight* light = new chai3d::cDirectionalLight(chai_world_);
+        if(S_NULL==light)
+        { throw(std::runtime_error("Couldn't add a light to the world")); }
+        light->m_userName = std::string("scl_id_light_in_world");
+
+        chai_cam_->addChild(light);                   // attach light to camera
+        light->setEnabled(true);                   // enable light source
+
+        chai3d::cVector3d pos( it->pos_[0], it->pos_[1], it->pos_[2]);
+        chai3d::cVector3d lookat( it->lookat_[0], it->lookat_[1], it->lookat_[2]);
+
+        light->setLocalPos(pos);  // position the light source
+        light->setDir(lookat-pos);  // define the direction of the light beam
+
+#ifdef DEBUG
+        std::cout<<"\nLight Pos    :"<<pos(0)<<" "<<pos(1)<<" "<<pos(2);
+        std::cout<<"\nLight Lookat :"<<lookat(0)<<" "<<lookat(1)<<" "<<lookat(2);
+        std::cout<<"\nLight Dir    :"<<lookat(0)-pos(0)<<" "<<lookat(1)-pos(1)<<" "<<lookat(2)-pos(2)<<std::endl;
+#endif
+      }
+    }
+    catch(std::exception& ee)
+    {
+      std::cerr<<"\nSGraphicsChai::init() : "<<ee.what();
+      return false;
+    }
+    has_been_init_ = true;
+    return true;
+  }
+
+  /** Constructor : Sets stuff to NULL. */
+  SGraphicsChai::SGraphicsChai() : SObject(std::string("SGraphicsChai"))
+  {
+    //For drag and click
+    mouse_grab_pos_ = Eigen::Vector3d::Zero();
+  }
+
+}
--- src/scl/graphics/chai/data_structs/SGraphicsChai.hpp
+++ src/scl/graphics/chai/data_structs/SGraphicsChai.hpp
@@ -33,7 +33,7 @@ scl. If not, see <http://www.gnu.org/licenses/>.
 
 #include <scl/DataTypes.hpp>
 #include <scl/data_structs/SObject.hpp>
-#include <scl/data_structs/SRobotIO.hpp>
+#include <scl/data_structs/SGraphicsParsed.hpp>
 #include <scl/graphics/chai/data_structs/SGraphicsChaiRigidBody.hpp>
 #include <scl/graphics/chai/data_structs/SGraphicsChaiMuscleSet.hpp>
 
@@ -74,14 +74,36 @@ public:
 class SGraphicsChai : public SObject
 {
 public:
+  /** An initialization function. Note, calling this will reset the whole state by default..
+   *
+   * Q) Why do we support resets without erasing chai state?
+   * A) We do support reinitializing this without resetting all the chai objects. This
+   * is because we might want to serialize/deserialize scl graphics configurations and reset
+   * the views without wanting to directly edit chai variables.
+   * In this case, it is useful to simply call init again on the SGraphicsChai object without
+   * having to reinitialize the entire chai data (simply an update).
+   * This is useful because the parsed graphics data is basically a scene specification. And
+   * it doesn't contain misc. added robots, meshes etc. Resetting the chai data would mean that
+   * we'd have to re-init all the added robots etc.---a waste of time.
+   * */
+  bool init(/** The graphics data structure loaded from the xml file. Specifies camera etc. information. */
+      const SGraphicsParsed* arg_gr_ds,
+      const bool arg_reset_chai_world=true);
+
+  /** Default constructor */
+  SGraphicsChai();
+
+  /** Virtual destructor */
+  virtual ~SGraphicsChai(){}
+
   /**
    * The graphics world :
    * A scenegraph of what will be rendered
    */
-  chai3d::cWorld* chai_world_;
+  chai3d::cWorld* chai_world_ = S_NULL;
 
   /** A camera that looks at the world. */
-  chai3d::cCamera* chai_cam_;
+  chai3d::cCamera* chai_cam_ = S_NULL;
 
   /** Container for all the robots to be rendered. */
   sutil::CMappedList<std::string, SRobotRenderObj> robots_rendered_;
@@ -93,46 +115,19 @@ public:
   sutil::CMappedList<std::string, SGraphicsChaiMuscleSet> muscles_rendered_;
 
   /** Gui properties */
-  bool mouse_mode_cam_; //manipulate the camera or forces
-  bool mouse_mode_move_scene_; ///< Translate the camera or forces
+  bool mouse_mode_cam_ = true; /// Manipulate the camera or forces
+  bool mouse_mode_move_scene_ = false; /// Translate the camera or forces
 
-  sFloat gl_width_, gl_height_;
-  bool mouse_button_pressed_;
-  sInt mouse_x_, mouse_y_, mouse_button_;
+  sFloat gl_width_ = 1080, gl_height_ = 800; ///The width and height of the default gl instance
+  bool mouse_button_pressed_=false; /// Default mouse button press state
+  sInt mouse_x_=0, mouse_y_=0, mouse_button_=0; /// Mouse button defaults
 
-  scl::sFloat mouse_grab_mag_; //magnitude
-  Eigen::Vector3d mouse_grab_pos_;
-  chai3d::cGenericObject* mouse_grab_obj_;
+  scl::sFloat mouse_grab_mag_=0.0; /// Mouse grab scaling factor
+  Eigen::Vector3d mouse_grab_pos_; /// The position at which the mouse grab started
+  chai3d::cGenericObject* mouse_grab_obj_=NULL; /// The object grabbed
 
   /** Running or not */
-  sFloat running_;
-
-  /** Constructor : Sets stuff to NULL. */
-  SGraphicsChai() : SObject(std::string("SGraphicsChai"))
-  {
-    chai_cam_ = S_NULL;
-    chai_world_ = S_NULL;
-    has_been_init_ = false;
-    mouse_mode_cam_ = true;
-    mouse_mode_move_scene_=false;
-
-    //DEFAULTS
-    gl_width_ = 1080;
-    gl_height_ = 800;
-    running_ = false;
-
-    //Mouse defaults
-    mouse_mode_cam_ = true;
-    mouse_button_pressed_ = false;
-    mouse_x_ = 0;
-    mouse_y_ = 0;
-    mouse_button_ = 0;
-
-    //For drag and click
-    mouse_grab_mag_ = 0.0;
-    mouse_grab_pos_ = Eigen::Vector3d::Zero();
-    mouse_grab_obj_ = NULL;
-  }
+  sFloat running_=false;
 };
 
 }
