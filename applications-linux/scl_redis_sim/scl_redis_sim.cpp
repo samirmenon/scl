109e32ec5756c9927c300166d03661c023b4ce2e
Samir <smenon@cs.stanford.edu>
2016-07-10 15:50:59 -0700

    Pushing q and dq to redis

--- applications-linux/scl_redis_sim/scl_redis_sim.cpp
+++ applications-linux/scl_redis_sim/scl_redis_sim.cpp
@@ -38,8 +38,10 @@ scl. If not, see <http://www.gnu.org/licenses/>.
 //sutil clock.
 #include <sutil/CSystemClock.hpp>
 
-//Eigen 3rd party lib
+// 3rd party libs
 #include <Eigen/Dense>
+#include <hiredis/hiredis.h>
+
 //Standard includes
 #include <sstream>
 #include <iostream>
@@ -52,6 +54,18 @@ scl. If not, see <http://www.gnu.org/licenses/>.
 // Handle ctrl+c
 bool flag_sim_enabled=true;
 void handleExit(int s) { flag_sim_enabled=false; }
+
+/** Basic data for reading from and writing to a redis database...
+ * Makes it easy to keep track of things..*/
+class SHiredisStruct_RobotSim{
+public:
+  redisContext *context_ = NULL;
+  redisReply *reply_ = NULL;
+  const char *hostname_ = "127.0.0.1";
+  const int port_ = 6379;
+  const timeval timeout_ = { 1, 500000 }; // 1.5 seconds
+};
+
 /**
  * A sample server application to simulate the physics of a robot and provide a
  * redis interface similar to the standard scl robot driver(s) using redis.
@@ -61,6 +75,9 @@ int main(int argc, char** argv)
   std::cout<<"\n*******************************************************\n";
   std::cout<<  "              SCL Redis Robot Simulator";
   std::cout<<"\n*******************************************************\n";
+  std::cout<<"\n NOTE : This application assumes a default redis server is "
+             <<"\n        running on the standard port (6379) and that "
+             <<"\n        appropriate keys are set";
 
   // Set up terminate handler.
   struct sigaction sigIntHandler;
@@ -91,6 +108,7 @@ int main(int argc, char** argv)
       scl::SRobotIO rio;         //I/O data structure
       scl_ext::CDynamicsSclSpatial dyn_scl_sp; //Robot physics integrator...
       scl::CParserScl p;         //This time, we'll parse the tree from a file...
+      SHiredisStruct_RobotSim redis_ds; //The data structure we'll use for redis comm.
 
       double sim_dt = 0.001;     //Simulation timestep..
 
@@ -119,24 +137,79 @@ int main(int argc, char** argv)
       flag = flag && dyn_scl_sp.init(rds);      //Set up integrator object
       if(false == flag){ throw(std::runtime_error("Could not initialize data structures"));  } //Error check.
 
-      std::cout<<"\nInitialized data structures, starting physics integrator and redis communication."
-               <<"\nRunning. Press 'ctrl+c' to exit. Open redis-cli and type 'monitor' to see what it's doing)...\n"<<std::flush;
+      std::cout<<"\nInitialized data structures, starting physics integrator and redis communication.";
+
+      /******************************Redis Initialization************************************/
+      std::cout<<"\n The default REDIS keys used are: ";
+      std::cout<<"\n  scl::robot::"<<robot_name<<"::sensors::q";
+      std::cout<<"\n  scl::robot::"<<robot_name<<"::sensors::dq";
+      std::cout<<"\n  scl::robot::"<<robot_name<<"::actuators::fgc";
+      std::cout<<"\n  scl::robot::"<<robot_name<<"::torques_enabled";
+      std::cout<<"\n  scl::robot::"<<robot_name<<"::dof";
+      std::cout<<"\n  (note some drivers also require) scl::robot::"<<robot_name<<"::operate_mode";
+
+      char rstr[1024], rstr_robot_base[1024]; //For redis key formatting
+
+      sprintf(rstr_robot_base, "scl::robot::%s",robot_name.c_str());
+
+      redis_ds.context_= redisConnectWithTimeout(redis_ds.hostname_, redis_ds.port_, redis_ds.timeout_);
+      if (redis_ds.context_ == NULL) { throw(std::runtime_error("Could not allocate redis context."));  }
+
+      if(redis_ds.context_->err)
+      {
+        std::string err = std::string("Could not connect to redis server : ") + std::string(redis_ds.context_->errstr);
+        redisFree(redis_ds.context_);
+        throw(std::runtime_error(err.c_str()));
+      }
+
+      // PING server to make sure things are working..
+      redis_ds.reply_ = (redisReply *)redisCommand(redis_ds.context_,"PING");
+      std::cout<<"\n\n Redis : Pinged Redis server. Reply is, "<<redis_ds.reply_->str<<"\n";
+      freeReplyObject((void*)redis_ds.reply_);
+
+      // REDIS IO : Add robot to set of active robots..
+      redis_ds.reply_ = (redisReply *)redisCommand(redis_ds.context_, "SADD %s %s","scl::robots",robot_name.c_str());
+      freeReplyObject((void*)redis_ds.reply_);
+
+      // REDIS IO : Add DOF key..
+      redis_ds.reply_ = (redisReply *)redisCommand(redis_ds.context_, "SET %s::dof %d", rstr_robot_base, static_cast<int>(rio.dof_));
+      freeReplyObject((void*)redis_ds.reply_);
+
+      std::cout<<"\n ** To monitor Redis messages, open a redis-cli and type 'monitor' **";
 
       /******************************Main Loop************************************/
+      std::cout<<"\nRunning. Press 'ctrl+c' to exit...\n"<<std::flush;
       scl::sFloat t_start, t_end;
-
       t_start = sutil::CSystemClock::getSysTime();
 
       while(flag_sim_enabled)
       {
+        // The physics integrator
         sutil::CSystemClock::tick(sim_dt);
         flag = dyn_scl_sp.integrate(rgcm, rio, sim_dt); // Run the integrator with a 1ms timestep..
         //rob_io_ds->sensors_.dq_ -= rob_io_ds->sensors_.dq_/100000;
 
-        //std::cout<<"\n q: "<<rio.sensors_.q_.transpose();
+        // REDIS IO : Set q
+        { std::stringstream ss; ss<< rio.sensors_.q_.transpose(); sprintf(rstr, "%s", ss.str().c_str());  }
+        redis_ds.reply_ = (redisReply *)redisCommand(redis_ds.context_, "SET %s::sensors::q %s",rstr_robot_base,rstr);
+        freeReplyObject((void*)redis_ds.reply_);
+
+        // REDIS IO : Set dq
+        { std::stringstream ss; ss<< rio.sensors_.dq_.transpose(); sprintf(rstr, "%s", ss.str().c_str());  }
+        redis_ds.reply_ = (redisReply *)redisCommand(redis_ds.context_, "SET %s::sensors::dq %s",rstr_robot_base,rstr);
+        freeReplyObject((void*)redis_ds.reply_);
       }
 
       t_end = sutil::CSystemClock::getSysTime();
+
+      /******************************Redis Shutdown (remove keys)************************************/
+      // REDIS IO : Remove robot from set of active robots..
+      redis_ds.reply_ = (redisReply *)redisCommand(redis_ds.context_, "SREM %s %s","scl::robots",robot_name.c_str());
+      freeReplyObject((void*)redis_ds.reply_);
+
+      // REDIS IO : Remove all keys..
+      redis_ds.reply_ = (redisReply *)redisCommand(redis_ds.context_, "DEL %s::dof", rstr_robot_base); freeReplyObject((void*)redis_ds.reply_);
+      redis_ds.reply_ = (redisReply *)redisCommand(redis_ds.context_, "DEL %s::q", rstr_robot_base); freeReplyObject((void*)redis_ds.reply_);
 
       /****************************Print Collected Statistics*****************************/
       //Now you can get the energies
