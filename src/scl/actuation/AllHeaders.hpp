f4de0fd8e888994aff8c55fdf9a7cb560106a39b
Samir <smenon@cs.stanford.edu>
2016-07-17 00:05:23 -0700

    Adding actuation headers

---
+++ src/scl/actuation/AllHeaders.hpp
@@ -0,0 +1,44 @@
+/* This file is part of scl, a control and simulation library
+for robots and biomechanical models.
+
+scl is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+Alternatively, you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of
+the License, or (at your option) any later version.
+
+scl is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License and a copy of the GNU General Public License along with
+scl. If not, see <http://www.gnu.org/licenses/>.
+*/
+/* \file AllHeaders.hpp
+ *
+ *  Created on: Jul 16, 2016
+ *
+ *  Copyright (C) 2016
+ *
+ *  Author: Samir Menon <smenon@stanford.edu>
+ */
+
+#ifndef SRC_SCL_ACTUATION_ALLHEADERS_HPP_
+#define SRC_SCL_ACTUATION_ALLHEADERS_HPP_
+
+// NOTE : These are not derived from a base class and are used as a set (not individually).
+#include <scl/actuation/muscles/CActuatorMuscle.hpp>
+#include <scl/actuation/muscles/data_structs/SActuatorMuscle.hpp>
+// NOTE : These are derived from a base class and so can be up-cast.
+#include <scl/actuation/muscles/CActuatorSetMuscle.hpp>
+#include <scl/actuation/muscles/data_structs/SActuatorSetMuscle.hpp>
+
+
+
+#endif /* SRC_SCL_ACTUATION_ALLHEADERS_HPP_ */
