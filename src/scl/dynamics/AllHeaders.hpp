02f5bac344c8019f21697664feca9498e71bb337
Samir <smenon@cs.stanford.edu>
2016-07-17 00:27:33 -0700

    More headers

---
+++ src/scl/dynamics/AllHeaders.hpp
@@ -0,0 +1,42 @@
+/* This file is part of scl, a control and simulation library
+for robots and biomechanical models.
+
+scl is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+Alternatively, you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of
+the License, or (at your option) any later version.
+
+scl is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License and a copy of the GNU General Public License along with
+scl. If not, see <http://www.gnu.org/licenses/>.
+*/
+/* \file AllHeaders.hpp
+ *
+ *  Created on: Jul 16, 2016
+ *
+ *  Copyright (C) 2016
+ *
+ *  Author: Samir Menon <smenon@stanford.edu>
+ */
+
+#ifndef SRC_SCL_DYNAMICS_ALLHEADERS_HPP_
+#define SRC_SCL_DYNAMICS_ALLHEADERS_HPP_
+
+// The standard dynamics class.
+#include <scl/dynamics/scl/CDynamicsScl.hpp>
+
+// Analytic dynamics for certain robots.
+#include <scl/dynamics/analytic/CDynamicsAnalyticRPP.hpp>
+
+
+#endif /* SRC_SCL_DYNAMICS_ALLHEADERS_HPP_ */
