c8793c9cf485dd4b2d3f7494f994438702b05ef4
Samir <smenon@cs.stanford.edu>
2016-08-20 00:17:38 -0700

    Faster compile for callbacks

--- src/scl/robot/GenericCallbacks.hpp
+++ src/scl/robot/GenericCallbacks.hpp
@@ -33,10 +33,8 @@ scl. If not, see <http://www.gnu.org/licenses/>.
 #define GENERICCALLBACKS_HPP_
 
 #include <sutil/CRegisteredCallbacks.hpp>
-#include <sutil/CRegisteredPrintables.hpp>
 
 #include <string>
-#include <iostream>
 
 
 namespace scl
@@ -50,23 +48,12 @@ namespace scl
    * (An example for you) */
   class CCallbackEcho : public sutil::CCallbackBase<std::string, std::vector<std::string> >
   {
-    typedef sutil::CCallbackBase<std::string, std::vector<std::string> > base;
   public:
-    virtual void call(std::vector<std::string>& arg)
-    {//Print out all the arguments (except the first, which is the command)
-      if(1 >= arg.size()) {}
-      else if("--help" == arg[1])
-      { std::cout<<" >>echo\n Echoes a string back on to the screen"; }
-      else
-      {
-        std::vector<std::string>::iterator it, ite;
-        for(it = arg.begin()+1, ite = arg.end(); it != ite; ++it)
-        { std::cout<<*it<<" "; }
-      }
-    }
+    typedef sutil::CCallbackBase<std::string, std::vector<std::string> > base;
 
-    virtual base* createObject()
-    { return dynamic_cast<base*>(new CCallbackEcho()); }
+    virtual void call(std::vector<std::string>& arg);
+
+    virtual base* createObject();
   };
 
   /** Print out a specific data structure.
@@ -77,30 +64,12 @@ namespace scl
    * example of how)*/
   class CCallbackPrint : public sutil::CCallbackBase<std::string, std::vector<std::string> >
   {
-    typedef sutil::CCallbackBase<std::string, std::vector<std::string> > base;
   public:
-    virtual void call(std::vector<std::string>& arg)
-    {//Print out all the arguments (except the first, which is the command)
-      if(1 >= arg.size())
-      { std::cout<<"Object not found";  }
-      else if("--help" == arg[1])
-      { std::cout<<" >>print <object_name>\n Finds the object in the print registry and prints its contents"; }
-      else
-      {//Prints the contents of the object
-        std::vector<std::string>::iterator it, ite;
-        for(it = arg.begin()+1, ite = arg.end(); it != ite; ++it)
-        {
-          const sutil::SPrintableBase* obj = sutil::printables::get(*it);
-          if(NULL == obj)
-          { std::cout<<"Object not found";  }
-          else
-          { std::cout<<(*obj);  }
-        }
-      }
-    }
+    typedef sutil::CCallbackBase<std::string, std::vector<std::string> > base;
 
-    virtual base* createObject()
-    { return dynamic_cast<base*>(new CCallbackPrint()); }
+    virtual void call(std::vector<std::string>& arg);
+
+    virtual base* createObject();
   };
 
 
@@ -112,75 +81,46 @@ namespace scl
    * class)*/
   class CCallbackSet : public sutil::CCallbackBase<std::string, std::vector<std::string> >
   {
-    typedef sutil::CCallbackBase<std::string, std::vector<std::string> > base;
   public:
-    virtual void call(std::vector<std::string>& arg)
-    {//Set out all the arguments (except the first, which is the command)
-      if(1 >= arg.size())
-      { std::cout<<"Object not found";  }
-      else if("--help" == arg[1])
-      { std::cout<<" >>set <object_name> <arg_str1> <arg_str2> ... <arg_strN>"
-        <<"\n Sets the contents of an object in the database"; }
-      else
-      {//Sets the contents of the object
-        //const scl::SObject** obj = scl::CObjectMap::getData()->at_const(*(arg.begin()+1));
-        //if(NULL == obj) { std::cout<<"Object not found";  }
-        //else
-        //{ std::cout<<(*obj);  }
-      }
-    }
+    typedef sutil::CCallbackBase<std::string, std::vector<std::string> > base;
 
-    virtual base* createObject()
-    { return dynamic_cast<base*>(new CCallbackSet()); }
+    virtual void call(std::vector<std::string>& arg);
+
+    virtual base* createObject();
   };
 
   /** Prints out a generic help message and also lists all the
    * other available commands */
   class CCallbackHelp : public sutil::CCallbackBase<std::string, std::vector<std::string> >
   {
-    typedef sutil::CCallbackBase<std::string, std::vector<std::string> > base;
   public:
-    virtual void call(std::vector<std::string>& arg)
-    {//Help out all the arguments (except the first, which is the command)
-      std::vector<std::string> idxlist;
-      //Get a list of all the callbacks in the registry
-      if(false == sutil::callbacks::list(idxlist))
-      { std::cout<<"Callback registry is empty";  }
-      else
-      {
-        if(1 >= idxlist.size())
-        { std::cout<<"Available commands:"; }
-        std::vector<std::string>::iterator it,ite;
-        for(it = idxlist.begin(), ite = idxlist.end(); it!=ite; ++it)
-        { std::cout<<" "<<(*it); } //Print all the callbacks
-        std::cout<<"\nFor details, type: <command> --help\nTo switch to char mode, type: x\nTo exit, type: exit";
-      }
-    }
+    typedef sutil::CCallbackBase<std::string, std::vector<std::string> > base;
 
-    virtual base* createObject()
-    { return dynamic_cast<base*>(new CCallbackHelp()); }
+    virtual void call(std::vector<std::string>& arg);
+
+    virtual base* createObject();
   };
 
   /** Key hander callbacks for decrementing a double data member
    * (5x data change if key is caps) */
   class CCallbackDecrement : public sutil::CCallbackBase<char, bool, double>
   {
-    typedef sutil::CCallbackBase<char,bool,double> base;
   public:
+    typedef sutil::CCallbackBase<char,bool,double> base;
     CCallbackDecrement(){}
-    virtual void call(bool& arg) { if(arg) (*data_)-=0.1; else (*data_)-=0.02; }
-    virtual base* createObject()  { return dynamic_cast<base*>(new CCallbackDecrement()); }
+    virtual void call(bool& arg);
+    virtual base* createObject();
   };
 
   /** Key hander callbacks for incrementing a double data member
    * (5x data change if key is caps) */
   class CCallbackIncrement : public sutil::CCallbackBase<char, bool, double>
   {
-    typedef sutil::CCallbackBase<char,bool,double> base;
   public:
+    typedef sutil::CCallbackBase<char,bool,double> base;
     CCallbackIncrement() {}
-    virtual void call(bool& arg) { if(arg) (*data_)+=0.1; else (*data_)+=0.02; }
-    virtual base* createObject()  { return dynamic_cast<base*>(new CCallbackIncrement()); }
+    virtual void call(bool& arg);
+    virtual base* createObject();
   };
 }
 
