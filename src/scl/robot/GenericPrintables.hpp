6be0b93a462c00f68ea9f8f0ed2d42383b771b62
Samir <smenon@cs.stanford.edu>
2016-08-20 01:01:23 -0700

    Minor updates (we're going to remove these soon)

--- src/scl/robot/GenericPrintables.hpp
+++ src/scl/robot/GenericPrintables.hpp
@@ -32,91 +32,6 @@ scl. If not, see <http://www.gnu.org/licenses/>.
 #ifndef GENERICPRINTABLES_HPP_
 #define GENERICPRINTABLES_HPP_
 
-#include <scl/data_structs/SRobotIO.hpp>
-#include <scl/data_structs/SRigidBody.hpp>
-#include <scl/data_structs/SRobotParsed.hpp>
-#include <scl/control/data_structs/SControllerBase.hpp>
-#include <scl/control/task/data_structs/STaskBase.hpp>
-#include <scl/control/task/tasks/data_structs/STaskOpPos.hpp>
-#include <scl/control/task/tasks/data_structs/STaskGc.hpp>
-#include <scl/control/task/data_structs/SControllerMultiTask.hpp>
-
-#include <scl/Singletons.hpp>
-
-#include <sutil/CRegisteredPrintables.hpp>
-
-#include <stdexcept>
-#include <iomanip>
-
-#ifdef DEBUG
-#include <cassert>
-#endif
-
-/* **************************************************************************
- *                         Starting namespace sutil
- *
- * These are a set of printable object definitions for objects in the database
- * Creating separate printable objects enables accessing the objects's values
- * through callbacks
- * Directly accessing the objects might be hard for certain situations
- * (Like from another process or a gui) and callbacks are good there.
- * ************************************************************************* */
-namespace sutil
-{
-  template <>
-  void printToStream<scl::SRobotParsed>(
-      std::ostream& ostr,
-      const scl::SRobotParsed& arg_data
-  );
-
-  template <>
-  void printToStream<scl::SRobotIO>(
-      std::ostream& ostr,
-      const scl::SRobotIO& arg_data
-  );
-
-  template <>
-  void printToStream<scl::SRigidBody>(
-      std::ostream& ostr,
-      const scl::SRigidBody& arg_data
-  );
-
-  template <>
-  void printToStream<scl::STaskBase>(
-      std::ostream& ostr,
-      const scl::STaskBase& arg_data
-  );
-
-  template <>
-  void printToStream<scl::STaskOpPos>(
-      std::ostream& ostr,
-      const scl::STaskOpPos& arg_data
-  );
-
-  template <>
-  void printToStream<scl::STaskGc>(
-      std::ostream& ostr,
-      const scl::STaskGc& arg_data
-  );
-
-  template <>
-  void printToStream<scl::SControllerMultiTask>(
-      std::ostream& ostr,
-      const scl::SControllerMultiTask& arg_data
-  );
-
-  template <>
-  void printToStream<scl::SGcModel>(
-      std::ostream& ostr,
-      const scl::SGcModel& arg_data
-  );
-
-  template <>
-  void printToStream<Eigen::Vector3d >(
-      std::ostream& ostr,
-      const Eigen::Vector3d& arg_eigvec
-  );
-}
 
 /* **************************************************************************
  *                         Starting namespace scl
@@ -125,26 +40,6 @@ namespace sutil
  * ************************************************************************* */
 namespace scl
 {
-  /** This adds an object to the printable registry. That allows pretty-printing
-   * this object from the command line shell...
-   *
-   * NOTE : This function may throw an exception (not typical SCL) */
-  template <class T>
-  bool printableAddObject(const T & arg_obj);
-
-  /** This is to just add an object and member objects
-   * NOTE : This function may throw an exception (not typical SCL) */
-  template <>
-  bool printableAddObject<scl::SRobotParsed>(const scl::SRobotParsed & arg_obj);
-
-  /** This is to just add an object and member objects */
-  template <>
-  bool printableAddObject<scl::SRobotIO>(const scl::SRobotIO & arg_obj);
-
-  /** This is to just add a gc model object */
-  template <>
-  bool printableAddObject<scl::SGcModel>(const scl::SGcModel & arg_obj);
-
   bool addRobotPrintables();
 }
 #endif /* GENERICPRINTABLES_HPP_ */
