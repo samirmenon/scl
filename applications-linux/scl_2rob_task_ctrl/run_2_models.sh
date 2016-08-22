6581f39919fe5ba4c7e97abbf97781c01f37646b
Samir <smenon@cs.stanford.edu>
2016-08-22 01:42:01 -0700

    Fixing hand task for the first robot

--- applications-linux/scl_2rob_task_ctrl/run_2_models.sh
+++ applications-linux/scl_2rob_task_ctrl/run_2_models.sh
@@ -1,2 +1,2 @@
 rm *.log -rf &&
-./scl_2rob_ctrl ../../specs/Pr2/Pr2Cfg.xml Pr2Bot opc hand hand2 ./Pr2BCfg.xml Pr2BotB opcB handB hand2B 
+./scl_2rob_ctrl ../../specs/Pr2/Pr2Cfg.xml Pr2Bot opc hand hand2 ./Pr2BCfg.xml Pr2BotB opcB hand2B handB
