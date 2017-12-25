1d11b4ea0d32bfe64b7455e52c34d14bb999c1ab
Samir <smenon@cs.stanford.edu>
2017-12-25 14:00:44 -0800

    Cleaner make libs script (also chai should be compiled separately)

--- applications-linux/scl_lib/make_libs.sh
+++ applications-linux/scl_lib/make_libs.sh
@@ -6,19 +6,14 @@ cd ../../
 git submodule init && git submodule sync && git submodule init && git submodule update 
 
 # ***********************
-# Now we compile chai (the graphics lib)
-cd 3rdparty/chai3d.git/ 
-#rm -rf build_* 
-sh make_everything.sh
-
-# ***********************
 # Now we compile the otg
-cd ../../otgtypeii/Linux/
+cd 3rdparty/otgtypeii/Linux/
 sh make_everything.sh
+cd ../../../
 
 # ***********************
 # Now we compile scl lib
-cd ../../../applications-linux/scl_lib 
+cd applications-linux/scl_lib 
 #rm -rf build_* 
 sh make_debug.sh 
 sh make_release.sh 
