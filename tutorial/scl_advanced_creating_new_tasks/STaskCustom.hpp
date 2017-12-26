8451beff5c5a4648d68f1f180631d4333794dc69
Samir <smenon@cs.stanford.edu>
2017-12-25 18:57:18 -0800

    Free tutes.

--- tutorial/scl_advanced_creating_new_tasks/STaskCustom.hpp
+++ tutorial/scl_advanced_creating_new_tasks/STaskCustom.hpp
@@ -1,24 +1,8 @@
 /* This file is part of scl, a control and simulation library
 for robots and biomechanical models.
 
-scl is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 3 of the License, or (at your option) any later version.
-
-Alternatively, you can redistribute it and/or
-modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation; either version 2 of
-the License, or (at your option) any later version.
-
-scl is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License and a copy of the GNU General Public License along with
-scl. If not, see <http://www.gnu.org/licenses/>.
+This file is released under the MIT license.
+See COPYING.MIT in the scl base directory.
 */
 /* \file STaskCustom.hpp
  *
